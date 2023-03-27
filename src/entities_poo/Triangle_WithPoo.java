81db855d7c1f5c5ff7dd1f7cc95302fa13aaf6b9
Vinicius Silva <xdenik13@gmail.com>
2023-03-27 10:35:28 -0300

    criando metodo da area

--- src/entities_poo/Triangle_WithPoo.java
+++ src/entities_poo/Triangle_WithPoo.java
@@ -5,5 +5,10 @@ public class Triangle_WithPoo {
 	public double a;
 	public double b;
 	public double c;
+	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+	}
 
 }
