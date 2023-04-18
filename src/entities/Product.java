660e094a691e7f6baf90a9b14e33c2cef05b5a91
Vinicius Silva <xdenik13@gmail.com>
2023-04-18 15:03:09 -0300

    finalizando ex-fixação

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,13 +2,16 @@ package entities;
 
 public class Product {
 	
-	private String name;
+	String name;
 	private double price;
 	private int quantity;
 	
+	public Product() {
+		
+	}
 	
-	
-	public Product (String name, double price, int quantity) {
+	public Product(String name, double price, int quantity) {
+		super();
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
@@ -19,26 +22,31 @@ public class Product {
 		this.price = price;
 	}
 	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
 	public double totalValueInStock() {
 		return price * quantity;
 	}
