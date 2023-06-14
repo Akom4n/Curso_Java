07d1fa2cc202b41648e3c79734c43a1274ad03e2
Vinicius <xdenik13@gmail.com>
2023-06-14 19:57:01 -0300

    Bloco finally

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,43 +1,31 @@
 package application;
 
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		method1();
-		
-		System.out.println("Fim do programa!");
-		
-	}
-	
-	public static void method1() {
-		System.out.println("***METHOD1 START***");
-		method2();
-		System.out.println("***METHOD1 END***");
-	}
-	
-	public static void method2() {
-		System.out.println("***METHOD2 START***");
-		Scanner sc = new Scanner(System.in);
-		
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
 		try {
-			String[] vect = sc.nextLine().split(" ");
-			int position = sc.nextInt();
-			System.out.println(vect[position]);
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
 		}
-		catch(ArrayIndexOutOfBoundsException e) {
-			System.out.println("Invalid position");
-			e.printStackTrace();
+		catch(FileNotFoundException e ) {
+			System.out.println("Error opening file: " + e.getMessage());
 		}
-		catch(InputMismatchException e) {
-			System.out.println("Input error!");
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
 		}
 		
-		sc.close();
-		System.out.println("***METHOD2 END***");
 		
 	}
 
