eb24529aa08a3b074048c24f1974df7fc14260c0
Vinicius Silva <xdenik13@gmail.com>
2023-07-30 19:09:25 -0300

    testando novamente

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 	
 	public static void main(String[] args) {
 		//Zerando para modulo Generic/Set/Map
 		//Testando commit para github pois mudei para bspwm
+		//Commitando só para ter certeza que o push vai funcionar
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
 	} 
 }
