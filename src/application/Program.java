8a39cf3963bd3cdd64022c3e3d0591def3d0aeab
Vinicius Silva <xdenik13@gmail.com>
2023-07-31 19:44:41 -0300

    inicio generics

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,28 @@
 package application;
 
-import java.util.Locale;
 import java.util.Scanner;
+
+import service.PrintService;
+
 
 public class Program {
 	
 	public static void main(String[] args) {
-		//Zerando para modulo Generic/Set/Map
-		//Testando commit para github pois mudei para bspwm
-		//Commitando só para ter certeza que o push vai funcionar
-		
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		PrintService ps = new PrintService();
+		
+		System.out.print("How many values?");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			int value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		System.out.println("First: " + ps.first());
+		
 		
 		sc.close();
 	} 
