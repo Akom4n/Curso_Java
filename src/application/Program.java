e9f1ec65597544dbb0255a070e68406dac1cbda9
Vinicius Silva <xdenik13@gmail.com>
2023-08-10 22:09:38 -0300

    Primeira forma comparator

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,53 +1,23 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
 
 public class Program {
-	//pequena alteração para teste no github
 	
-	public static void main(String[] args) throws ParseException, FileNotFoundException, IOException {
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
 		
-		Scanner sc = new Scanner(System.in);
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
 		
-		Map<String, Integer> votes = new LinkedHashMap<>();
+		list.sort(new MyComparator());
 		
-		System.out.println("Enter file: ");
-		String path = sc.nextLine();
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			
-			String line = br.readLine();
-			while (line != null) {
-				
-				String[] fields = line.split(",");
-				String name = fields[0];
-				int count = Integer.parseInt(fields[1]);
-				
-				if (votes.containsKey(name)){
-					int votesSoFar = votes.get(name);
-					votes.put(name, count + votesSoFar);
-				}
-				else {
-					votes.put(name, count);
-				}
-				
-				line = br.readLine();
-			}
-			
-			for (String key : votes.keySet()) {
-				System.out.println(key + ": " + votes.get(key));
-			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		for (Product p : list) {
+			System.out.println(p);
 		}
-		
-		sc.close();
 	}
 }
