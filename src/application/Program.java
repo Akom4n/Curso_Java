6a0b19eb8a9d1b470ed0990637389eb692168935
Vinicius Silva <xdenik13@gmail.com>
2023-08-12 21:01:02 -0300

    Implementação interface predicate

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,25 +2,28 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
-	
-	public static int compareProducts(Product p1, Product p2) {
-		return p1.getPrice().compareTo(p2.getPrice());
-		}
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("TV", 900.00));
-		list.add(new Product("Notebook", 1200.00));
-		list.add(new Product("Tablet", 450.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("HD Case", 80.90));
 		
-		list.sort(Program::compareProducts);
+		list.removeIf(new ProductPredicate());
 		
-		list.forEach(System.out::println);
+		for (Product p : list) {
+			System.out.println(p);
+		}
+		
 	}
 }
