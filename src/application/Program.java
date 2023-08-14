5990909f58dccf9694feb191a274ca091b6fb1ff
Vinicius Silva <xdenik13@gmail.com>
2023-08-13 21:12:03 -0300

    Consumer

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import entities.Product;
+import util.PriceUpdate;
 
 public class Program {
 
@@ -19,12 +19,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
+		list.forEach(new PriceUpdate());
 		
-		list.removeIf(p -> p.getPrice() >= 100.0);
-		
-		for (Product p : list) {
-			System.out.println(p);
-		}
-		
+		list.forEach(System.out::println);
 	}
 }
