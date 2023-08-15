849186ad0a8f026ee2cc5fb7b2f77cc2eb6a5058
Vinicius Silva <xdenik13@gmail.com>
2023-08-15 20:03:00 -0300

    Function whit other function lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import entities.Product;
+import module.service.ProductService;
 
 public class Program {
 
@@ -20,8 +19,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		ProductService ps = new ProductService();
 		
-		names.forEach(System.out::println);
+		double sum = ps.filteredSum(list, p -> p.getPrice() < 100.00);
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
 	}
 }
